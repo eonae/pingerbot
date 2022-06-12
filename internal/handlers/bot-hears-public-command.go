23dbc3e99839edc49a2110d46038598a638f2054
eonae <eonae.white@gmail.com>
2022-06-13 00:00:23 +0300

    feat: /add + /addme commands

--- internal/handlers/bot-hears-public-command.go
+++ internal/handlers/bot-hears-public-command.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"fmt"
 	"pingerbot/internal/state"
 	tg "pingerbot/pkg/telegram"
 	"strings"
@@ -29,6 +30,33 @@ func (h BotHearsPublicCommand) Handle(ctx tg.CommandCtx) error {
 		}
 
 		return ctx.Reply(tg.OutgoingMessage{Text: strings.Join(members, " ")})
+	case "/add":
+		mentions := ctx.Mentions()
+
+		fmt.Println(mentions)
+
+		if len(mentions) == 0 {
+			return ctx.Reply(tg.OutgoingMessage{Text: "Please provide some usernames!"})
+		}
+
+		for _, mention := range mentions {
+			if string(mention[0]) != "@" {
+				ctx.Logger.Warn("Can't remember user %s - not username!", mention)
+				continue
+			}
+
+			// TODO: Batch add
+			ctx.Logger.Infof("Remembering user %s", mention)
+
+			// Strip @ because it will be added inside of RememberMember method
+			err := h.S.RememberMember(ctx.ChatId, mention[1:])
+			if err != nil {
+				ctx.Logger.Error(err)
+			}
+		}
+
+	case "/addme":
+		return rememberIfHasUsername(ctx.ChatId, ctx.Message.From, h.S, ctx.Logger, ctx)
 	default:
 		ctx.Logger.Warnf("Unknown command: %s", ctx.Command)
 	}
